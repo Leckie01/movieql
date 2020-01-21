ba0d4aa425dc4e8a7a1e8c0158ec44bc365e0d12
Leckie <hsk0094@gmail.com>
2020-01-21 18:36:33 +0900

    Wrapping a REST API with GraphQL Part One

--- graphql/db.js
+++ graphql/db.js
@@ -1,71 +1,16 @@
-export let movies = [
-  {
-    id: 0,
-    name: "Leckie",
-    age: 27,
-    gender: "male"
-  },
-  {
-    id: 1,
-    name: "Jisu",
-    age: 18,
-    gender: "female"
-  },
-  {
-    id: 2,
-    name: "Japan Guy",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: 3,
-    name: "Daal",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: 4,
-    name: "JD",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: 5,
-    name: "moondaddi",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: 6,
-    name: "flynn",
-    age: 18,
-    gender: "male"
-  }
-];
+import axios from "axios";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.am/api/v2/list_movies.json";
 
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === id);
-  return filteredMovies[0];
-};
+const api = axios.create({
+  baseURL: API_URL
+});
 
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await api.get("/", { limit, rating });
+  return movies;
 };
