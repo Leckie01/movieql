8cf851a32bd04a560a765a829e8acd4eb6a0d288
Leckie <hsk0094@gmail.com>
2020-01-21 19:38:14 +0900

    Overview to the final API

--- graphql/db.js
+++ graphql/db.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = "https://yts.am/api/v2/list_movies.json";
+const BASE_URL = "https://yts.am/api/v2/";
 
 const api = axios.create({
-  baseURL: API_URL
+  baseURL: BASE_URL
 });
 
 export const getMovies = async (limit, rating) => {
@@ -11,6 +11,31 @@ export const getMovies = async (limit, rating) => {
     data: {
       data: { movies }
     }
-  } = await api.get("/", { params: { limit, minimum_rating: rating } });
+  } = await api.get("/list_movies.json", {
+    params: { limit, minimum_rating: rating }
+  });
+  return movies;
+};
+
+export const getMovie = async id => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await api.get("/movie_details.json", {
+    params: {
+      movie_id: id
+    }
+  });
+  return movie;
+};
+
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await api.get("/movie_suggestions.json", { params: { movie_id: id } });
+
   return movies;
 };
